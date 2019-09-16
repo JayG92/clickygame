0b3198e16aecfee664b3e6dc902baca4f591782f
JayG <jaypguevara@gmail.com>
2019-09-16 07:52:44 -0700

    pushing

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -6,41 +6,42 @@ import cards from "./friends.json";
 import "./App.css";
 
 class App extends Component {
+
   state = {
     cards,
     score: 0,
     highscore: 0
   };
 
-  handleReset = () => {
-    if (this.state.score > this.state.highscore) {
-      this.setState({highscore: this.state.score}, function() {
-        console.log(this.state.highscore);
-      });
-    }
-    this.state.cards.forEach(card => {
-      card.count = 0;
-    });
-    alert("Game Over")
-    this.setState({score: 0});
-    return true;
-  }
-
   clickCount = id => {
-    this.state.cards.find((o, i) => {
-      if (o.id === id) {
+    this.state.cards.find((char, i) => {
+      if (char.id === id) {
+        // checks to see if card count is 0 and if so add +1
         if(cards[i].count === 0){
           cards[i].count = cards[i].count + 1;
           this.setState({score : this.state.score + 1}, function(){
-            console.log(this.state.score);
           });
-          this.state.cards.sort(() => Math.random() - 0.5)
+          // sorts cards
+          this.state.cards.sort(() => Math.random() - 1)
           return true; 
         } else {
           this.handleReset();
         }
       }
     });
+  }
+  handleReset = () => {
+    // Checks to see if score is greater than highscore
+    if (this.state.score > this.state.highscore) {
+      this.setState({highscore: this.state.score}, function() {
+      });
+    }
+    this.state.cards.forEach(card => {
+      card.count = 0;
+    });
+    alert("Game Over, You scored " + this.state.score)
+    this.setState({score: 0});
+    return true;
   }
 
   render() {
